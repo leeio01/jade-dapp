f833814c32c0bc7cde4f10356202b3cf092d587b
leeio01 <177035474+leeio01@users.noreply.github.com>
2025-08-26 00:33:18 +0500

    added headline

--- src/components/CosmicWaves.tsx
+++ src/components/CosmicWaves.tsx
@@ -3,9 +3,18 @@ import "./CosmicWaves.css";
 
 const CosmicWaves: React.FC = () => {
   return (
-    <div className="cosmic-wave">
-      <div className="wave"></div>
-      <div className="wave"></div>
+    <div className="waves-section">
+      <div className="cosmic-wave">
+        <div className="wave"></div>
+        <div className="wave"></div>
+
+        {/* Overlay Text ON the waves */}
+        <div className="waves-text">
+          Become a JADE
+          <br />
+          Early Investor
+        </div>
+      </div>
     </div>
   );
 };
